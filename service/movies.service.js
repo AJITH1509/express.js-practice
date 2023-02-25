eae2db122d66f3b4339c81fc792b63ef92e04512
AJITH1509 <ajithmuthu799@gmail.com>
2023-02-25 11:36:38 +0530

    add movie correction

--- service/movies.service.js
+++ service/movies.service.js
@@ -10,7 +10,7 @@ export async function deleteMovies(id) {
   return await client.db("b42wd2").collection("movies").deleteOne({ id: id });
 }
 export async function addMovie(data) {
-  return await client.db("b42wd2").collection("movies").insertMany(data);
+  return await client.db("b42wd2").collection("movies").insertOne(data);
 }
 export async function getMovieById(id) {
   return await client.db("b42wd2").collection("movies").findOne({ id: id });
