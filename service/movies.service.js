76d89796b76400aa2bd03cd3459b7b0295ff66b6
AJITH1509 <ajithmuthu799@gmail.com>
2023-03-02 22:50:58 +0530

    converted _id

--- service/movies.service.js
+++ service/movies.service.js
@@ -1,19 +1,26 @@
+import { ObjectId } from "mongodb";
 import { client } from "../index.js";
 
 export async function updateMovie(id, data) {
   return await client
     .db("b42wd2")
     .collection("movies")
-    .updateOne({ id: id }, { $set: data });
+    .updateOne({ _id: new ObjectId(id) }, { $set: data });
 }
 export async function deleteMovies(id) {
-  return await client.db("b42wd2").collection("movies").deleteOne({ id: id });
+  return await client
+    .db("b42wd2")
+    .collection("movies")
+    .deleteOne({ _id: new ObjectId(id) });
 }
 export async function addMovie(data) {
   return await client.db("b42wd2").collection("movies").insertOne(data);
 }
 export async function getMovieById(id) {
-  return await client.db("b42wd2").collection("movies").findOne({ id: id });
+  return await client
+    .db("b42wd2")
+    .collection("movies")
+    .findOne({ _id: new ObjectId(id) });
 }
 export async function getMovies() {
   return await client.db("b42wd2").collection("movies").find({}).toArray();
